bb7fcc8b7839947c4c90238322cf485dafbf3f4c
Ðîîñòèñëà Óñïàñêèé <ruspasskiy2002@gmail.com>
2023-03-27 12:03:55 +0300

    add AuthLayoutComponent

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './components/login/login.component';
-
+import { AuthRoutingModule } from './auth-routing.module';
+import { WallComponent } from './layout/wall/wall.component';
+import { AuthLayoutComponent } from './layout/auth-layout/auth-layout.component';
 
 
 @NgModule({
-  declarations: [
-    LoginComponent
-  ],
+  declarations: [LoginComponent, AuthLayoutComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    AuthRoutingModule
   ]
 })
 export class AuthModule { }
