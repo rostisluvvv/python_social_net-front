92468f27f2f69db0dfa2346307583f8bf7a62345
Ðîîñòèñëà Óñïàñêèé <ruspasskiy2002@gmail.com>
2023-03-27 12:02:25 +0300

    add ProfileModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthModule } from '../auth/auth.module';
+import { ProfileModule } from '../profile/profile.module';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { AuthModule } from '../auth/auth.module';
     BrowserModule,
     AppRoutingModule,
     AuthModule,
+    ProfileModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
