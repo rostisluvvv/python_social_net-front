b1415415822636bebfc208167dd278ca947030b5
Ðîîñòèñëà Óñïàñêèé <ruspasskiy2002@gmail.com>
2023-03-27 13:54:34 +0300

    add new components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotfoundComponent } from '../shared/components/notfound/notfound.component';
+import { AppComponent } from './app.component';
+import { AuthRoutingModule } from '../auth/auth-routing.module';
+import { ProfileRoutingModule } from '../profile/profile-routing.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: AppComponent,
+  },
+
+  {
+    path: '**', component: NotfoundComponent
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+    AuthRoutingModule,
+    ProfileRoutingModule,
+
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
