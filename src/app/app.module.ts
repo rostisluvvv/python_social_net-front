9ea06e8199ea36ed6deab43aad054b02b8935979
Ðîîñòèñëà Óñïàñêèé <ruspasskiy2002@gmail.com>
2023-03-27 11:19:33 +0300

    added changes in  structure project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AuthModule } from './auth/auth.module';
+import { AuthModule } from '../auth/auth.module';
 
 @NgModule({
   declarations: [
