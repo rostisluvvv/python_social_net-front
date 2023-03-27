7967a68323cde6f4d361f96e97889a2d679cff88
Ðîîñòèñëà Óñïàñêèé <ruspasskiy2002@gmail.com>
2023-03-27 19:58:25 +0300

    Add ngOnInit

--- src/auth/components/login/login.component.ts
+++ src/auth/components/login/login.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import {FormControl, Validators, FormGroup} from '@angular/forms';
+import {ErrorStateMatcher} from '@angular/material/core';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
 
+export class LoginComponent implements OnInit {
+
+  form: FormGroup;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      username: new FormControl(
+        null, [Validators.required]
+      ),
+      password: new FormControl(
+        null, [Validators.required, Validators.minLength(4)]
+      ),
+    });
+  }
+  submit(): void {
+  }
 }
