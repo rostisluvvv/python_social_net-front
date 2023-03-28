e626d95b9587c019c3d7ac1581b72a2c68dd51c7
Ðîîñòèñëà Óñïàñêèé <ruspasskiy2002@gmail.com>
2023-03-28 16:36:19 +0300

    add VerifyComponent + path

--- src/auth/auth-routing.module.ts
+++ src/auth/auth-routing.module.ts
@@ -4,6 +4,7 @@ import { LoginComponent } from './components/login/login.component';
 import { AuthLayoutComponent } from './layout/auth-layout/auth-layout.component';
 import { RegistrationComponent } from './components/registration/registration.component';
 import { LoginLayoutComponent } from './layout/login-layout/login-layout.component';
+import { VerifyComponent } from './verify/verify.component';
 
 const routes: Routes = [
   {
@@ -17,9 +18,14 @@ const routes: Routes = [
           },
           {
             path: 'registration', component: RegistrationComponent
-          }
+          },
         ]
       },
+
+      {
+        path: 'activate/:uid/:token', component: VerifyComponent
+      },
+
     ]
   },
 ];
