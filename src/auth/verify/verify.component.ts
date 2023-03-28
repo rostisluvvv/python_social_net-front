e8b62a7389c6c97001a2789a12140a1ed9421608
Ðîîñòèñëà Óñïàñêèé <ruspasskiy2002@gmail.com>
2023-03-28 16:35:14 +0300

    add component

--- src/auth/verify/verify.component.ts
+++ src/auth/verify/verify.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { Subscription } from 'rxjs';
+import {ActivatedRoute} from '@angular/router';
+import {Verify} from '../models';
+
+
 
 @Component({
   selector: 'app-verify',
@@ -6,5 +12,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./verify.component.css']
 })
 export class VerifyComponent {
+
+  confirm = false;
+  verify: Verify;
+  ver = {token: '', uid: ''};
+  aSub: Subscription;
+  rSub: Subscription;
+
+  constructor(private authService: AuthService, private router: ActivatedRoute) {
+        this.ver.token = this.router.snapshot.params['token'];
+        this.ver.uid  = this.router.snapshot.params['uid'];
+  }
+
+
+  ngOnInit(): void {
+    this.send();
+  }
+
+  send(): void {
+    this.aSub = this.authService.verify(this.ver).subscribe(
+      res => this.confirm = true,
+      error => this.confirm = false
+    );
+  }
 
 }
