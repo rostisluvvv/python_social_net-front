0c1200451c36b4507713a126edc6f40a3cfc3ba4
Ðîîñòèñëà Óñïàñêèé <ruspasskiy2002@gmail.com>
2023-03-28 14:59:04 +0300

    change registration component

--- src/auth/components/registration/registration.component.ts
+++ src/auth/components/registration/registration.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../auth.service';
+import { Subscription } from 'rxjs';
+
+
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.css']
 })
-export class RegistrationComponent {
+export class RegistrationComponent implements OnInit {
+
+  form: FormGroup
+  aSub: Subscription;
+  errorRes: object | null = null
+
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit(): void {
+      this.form = new FormGroup({
+      username: new FormControl(
+        null, [Validators.required]
+      ),
+      email: new FormControl(
+        null, [Validators.required, Validators.email]
+      ),
+      password: new FormControl(
+        null, [Validators.required, Validators.minLength(8)]
+      ),
+    });
+  }
+
+  submit (): void {
+
+    this.aSub = this.authService.registration(this.form.value).subscribe(
+      res => alert('Send email'),
+      error => console.log(error.error)
+    );
+
+  }
 
 }
