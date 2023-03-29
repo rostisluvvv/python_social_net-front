8e74d766879ed5de730f1a470dd09258f3d7ab11
Ðîîñòèñëà Óñïàñêèé <ruspasskiy2002@gmail.com>
2023-03-29 11:18:54 +0300

    small fixes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -21,6 +21,7 @@ const routes: Routes = [
     AuthRoutingModule,
     ProfileRoutingModule,
 
+
   ],
   exports: [RouterModule]
 })
--- src/profile/profile.module.ts
+++ src/profile/profile.module.ts
@@ -3,16 +3,25 @@ import { CommonModule } from '@angular/common';
 
 import { ProfileRoutingModule } from './profile-routing.module';
 import { WallComponent } from './components/wall/wall.component';
+import { EditProfileComponent } from './components/edit-profile/edit-profile.component';
+import { BaseLayoutComponent } from './layout/base-layout/base-layout.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 
 @NgModule({
   declarations: [
-    WallComponent
+    WallComponent,
+    EditProfileComponent,
+    BaseLayoutComponent
+
   ],
   imports: [
     CommonModule,
     ProfileRoutingModule,
+    ReactiveFormsModule
 
-  ]
+
+  ],
 })
 export class ProfileModule { }
