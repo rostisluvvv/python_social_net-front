18a02b7a58556f6ae8b437fe9c0ba3cc91706491
Ðîîñòèñëà Óñïàñêèé <ruspasskiy2002@gmail.com>
2023-03-29 14:54:39 +0300

    add isAuth, getToken, setToken

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,26 +3,46 @@ import { HttpClient } from '@angular/common/http';
 import { environment } from '../environments/environment';
 import { Observable } from 'rxjs';
 import { Login, Registration, Token, Verify } from './models';
+import { tap } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
+  private token: string = '';
+
   constructor(private http: HttpClient) { }
 
   login(model: Login): Observable<Token> {
     return this.http.post<Token>(`${environment.url}/auth/jwt/create/`, model);
+}
+
+  registration(model: Registration): Observable<any> {
+      return this.http.post<any>(`${environment.url}/auth/users/`, model).pipe(
+        tap((token: Token) => {
+          this.setToken(token.access);
+          localStorage.setItem('token', token.access);
+      })
+    );
   }
 
-   registration(model: Registration): Observable<any> {
-    return this.http.post<any>(`${environment.url}/auth/users/`, model);
-  }
-
-
-  verify (model: Verify): Observable<any> {
+  verify(model: Verify): Observable<any> {
     return this.http.post<any>(`${environment.url}/auth/users/activation/`, model);
   }
 
+  isAuth(): boolean {
+     this.setToken(this.getToken());
+     return !!this.token;
+  }
+
+  getToken(): string{
+    return localStorage.getItem('token')||'';
+  }
+
+  setToken(token: string): void {
+    this.token = token;
+  }
 
 }
