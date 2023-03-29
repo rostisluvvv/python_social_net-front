2f163479a5807ee3c9242d71907ee1c5eb9748eb
Ðîîñòèñëà Óñïàñêèé <ruspasskiy2002@gmail.com>
2023-03-29 11:20:08 +0300

    add new path

--- src/profile/profile-routing.module.ts
+++ src/profile/profile-routing.module.ts
@@ -1,12 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { WallComponent } from './components/wall/wall.component';
+import { BaseLayoutComponent } from './layout/base-layout/base-layout.component';
+import { EditProfileComponent } from './components/edit-profile/edit-profile.component';
 
 
 const routes: Routes = [
   {
-    path: 'wall', component: WallComponent
-  }
+    path: 'profile', component: BaseLayoutComponent, children: [
+
+      {
+        path: 'edit', component: EditProfileComponent
+      },
+
+      {
+        path: 'wall', component: WallComponent
+      },
+
+    ]
+  },
+
+
 ];
 
 @NgModule({
